715138dcf875b57ceba56fc7e3c8d874f865a617
mattybraes <matthewmeiklebraes@gmail.com>
2026-06-10 10:14:45 +0200

    making button go to second screen

--- app/src/main/java/com/example/imad_practicum/MainscreenActivity.kt
+++ app/src/main/java/com/example/imad_practicum/MainscreenActivity.kt
@@ -1,42 +1,16 @@
 package com.example.imad_practicum
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val button=findViewById<Button>(R.id.switchButton) //find the button id
-        button.setOnClickListener {
-            val intent = Intent(this, mainScreen::class.java)
-            startActivity (Intent)
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        setContentView(R.layout.mainscreen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
